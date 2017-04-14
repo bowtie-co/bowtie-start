6a85ec32332f226dd5f61619a69a4223b51b4e35
Ian Golden <ian@iangolden.com>
2017-04-14 10:56:33 -0600

    Added repo_url test

--- test/bowtie_start_test.rb
+++ test/bowtie_start_test.rb
@@ -8,4 +8,9 @@ class BowtieStartTest < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def test_if_repo_url_is_valid
+    url = valid_url?('https://github.com/igolden/dotfiles')  
+    url === true ? (assert true) : (assert false)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'minitest/autorun'
+require "net/http"
+
 require 'bowtie_start'
 require 'bowtie_start/version'
-require 'minitest/autorun'
+
+def valid_url?(url)
+    url = URI.parse(url)
+    req = Net::HTTP.new(url.host, url.port); req.use_ssl = true
+    res = req.request_head(url.path)
+    case res.code
+    when "200"
+      return true
+    else
+      return false
+    end
+end
+
