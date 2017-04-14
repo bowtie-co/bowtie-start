b3f20fdb2dd5024cf0c1d50490f29298c7f050db
Ian Golden <ian@iangolden.com>
2017-04-14 10:26:32 -0600

    Checking in new test files

--- test/bowtie_start_test.rb
+++ test/bowtie_start_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class BowtieStarterTest < Minitest::Test
+class BowtieStartTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::BowtieStarter::VERSION
+    refute_nil BowtieStart::VERSION
   end
 
   def test_it_does_something_useful
